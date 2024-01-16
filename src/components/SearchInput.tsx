f7dca4ef521ec3ce5aa706e0ba9bc5e951eefedb
adamass <43586138+adamass@users.noreply.github.com>
2024-01-16 14:53:09 +0100

    Search input width edited.

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -31,7 +31,7 @@ const SearchInput = () => {
     console.log("Empty");
   };
 
-  return windowSize[0] < 768 ? (
+  return windowSize[0] < 400 ? (
     <form
       onClick={() => handleSearchInput()}
       onSubmit={(event) => {
@@ -53,6 +53,33 @@ const SearchInput = () => {
               variant="filled"
               width="7rem"
               className="pe-0"
+              onClick={() => setInputValue("")}
+            />
+          </InputGroup>
+        </Nav.Link>
+      </Nav>
+    </form>
+  ) : windowSize[0] < 528 ? (
+    <form
+      onClick={() => handleSearchInput()}
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) {
+          setInputValue(ref.current.value);
+          ref.current.value = "";
+        }
+      }}
+    >
+      <Nav>
+        <Nav.Link to="/store" as={NavLink}>
+          <InputGroup>
+            <InputLeftElement children={<BsSearch />} />
+            <Input
+              ref={ref}
+              borderRadius={20}
+              placeholder="Search..."
+              variant="filled"
+              width="12rem"
               onClick={() => setInputValue("")}
             />
           </InputGroup>
